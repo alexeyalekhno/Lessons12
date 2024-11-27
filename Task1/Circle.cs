26762a52d6f3e9df7f1f27c2bc2d12582aad84d1
mrale <mrale@DESKTOP-HHS76V9>
2024-11-27 22:37:11 +0300

    Добавьте файлы проекта.

---
+++ Task1/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    static class Circle
+    {
+        static public double GetLength(double r)
+        { 
+        return 2*Math.PI*r;
+        }
+
+        static public double GetSquare(double r)
+        {
+            return Math.PI * (r * r);
+        }
+
+        static public string GetCoordinates(double r, double x0, double x1, double y0, double y1)
+        {
+            if (Math.Sqrt(Math.Pow(x1 - x0, 2) + Math.Pow(y1 - y0, 2)) <= r)
+                return "принадлежат окружности";
+            else
+                return "не принадлежат окружности";
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности:");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки Х0");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки Х1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки Y1");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки Y2");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double lenght = Circle.GetLength(r);
+            double square = Circle.GetSquare(r);
+            string coordinates = Circle.GetCoordinates(r, x0, x1, y0, y1);
+            Console.WriteLine($"Длина окружности: {lenght}");
+            Console.WriteLine($"Площадь окружности равна {square}");
+            Console.WriteLine($"Координаты {coordinates}");
+            Console.ReadKey();
+        }
+    }
+}
